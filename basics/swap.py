8d18864df5cbbf38e33d9b25c0e71c1d9082564c
subhanshugithub <vermasubhanshu.sv.1999@gmail.com>
2020-10-01 14:26:44 +0530

    added a basic swap program

---
+++ basics/swap.py
@@ -0,0 +1,6 @@
+#A basic swap programming program made in python with just few lines of code 
+a=int(input("ENTER NUMBER 1: "))
+b=int(input("ENTER NUMBER 2: "))
+a,b=b,a
+print("NUMNBER 1 :",a)
+print("NUMBER 2 :",b)
